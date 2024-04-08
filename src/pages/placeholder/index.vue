33eaa1d290076ecdb800525101725b9e20f648b0
adolf Jiang <jiangjianhao1997@163.com>
2024-04-08 13:15:09 +0800

    style(placeholder): style center in placeholder page

--- src/pages/placeholder/index.vue
+++ src/pages/placeholder/index.vue
@@ -2,7 +2,7 @@
  * @Author: jiangjianhao1997@163.com
  * @Date: 2024-04-07 13:29:01
  * @LastEditors: adolf Jiang jiangjianhao1997@163.com
- * @LastEditTime: 2024-04-07 16:15:22
+ * @LastEditTime: 2024-04-07 17:03:20
  * @FilePath: /oiltrack-management/src/pages/placeholder/index.vue
  * @Description: placeholder page
  * Copyright (c) 2024 by mxj, All Rights Reserved.
@@ -17,8 +17,8 @@ definePage({
 </script>
 
 <template>
-  <div class="home-container" h-100vh bg-layout-background bg-contain bg-no-repeat p-15vw>
-    <span>手机号绑定成功</span>
+  <div class="home-container" h-100vh flex items-center justify-center bg-layout-background bg-contain bg-no-repeat p-15vw>
+    <span c-black>手机号绑定成功!</span>
   </div>
 </template>
 
