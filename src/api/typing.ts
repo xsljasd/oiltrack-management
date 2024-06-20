3f458c95d3181efb6db06d15bfb36671bab9526b
adolf Jiang <jiangjianhao1997@163.com>
2024-06-20 21:40:55 +0800

    feat: update

--- src/api/typing.ts
+++ src/api/typing.ts
@@ -2,7 +2,7 @@
  * @Author: jiangjianhao1997@163.com
  * @Date: 2024-03-08 14:20:17
  * @LastEditors: adolf Jiang jiangjianhao1997@163.com
- * @LastEditTime: 2024-04-06 12:03:01
+ * @LastEditTime: 2024-05-09 20:05:07
  * @FilePath: /oiltrack-management/src/api/typing.ts
  * @Description: api types
  * Copyright (c) 2024 by mxj, All Rights Reserved.
@@ -33,8 +33,8 @@ export interface PostRegisterBodyType {
 }
 
 export interface PostLoginBodyType {
-  UserCode: string
-  UserPwd: string
+  UserCode?: string
+  UserPwd?: string
   code: string
   state: string
 }
--- src/pages/login/index.vue
+++ src/pages/login/index.vue
@@ -2,14 +2,15 @@
  * @Author: jiangjianhao1997@163.com
  * @Date: 2024-03-12 23:32:53
  * @LastEditors: adolf Jiang jiangjianhao1997@163.com
- * @LastEditTime: 2024-04-09 22:36:23
+ * @LastEditTime: 2024-05-10 20:30:50
  * @FilePath: /oiltrack-management/src/pages/login/index.vue
  * @Description: login and logon page
  * Copyright (c) 2024 by mxj, All Rights Reserved.
 -->
 <script setup lang="ts">
-import { postLogin } from '@/api'
+import { getLogin } from '@/api'
 import router from '@/router'
+import { handleApiSuccessNotExcept } from '@/utils/apiErrorHandler'
 
 definePage({
   name: 'login',
@@ -31,21 +32,23 @@ onMounted(() => {
   const urlQuery = route.query
   LoginData.code = urlQuery?.code?.toString() ?? ''
   LoginData.status = urlQuery?.status?.toString() ?? ''
-  if (LoginData.code && LoginData.status) {
+  if (!!LoginData.code && !!LoginData.status) {
     localStorage.setItem('author-code', LoginData.code)
     localStorage.setItem('author-status', LoginData.status)
   }
   else {
     LoginData.code = localStorage.getItem('author-code')
-    LoginData.status = localStorage.getItem('author-status')
+    LoginData.status = localStorage.getItem('author-status') || ''
+    if (LoginData.status)
+      LoginData.status = ''
+    console.error('Init LoginData:', LoginData)
   }
 })
 
 // TODO api connect
 function submit() {
-  postLogin({
-    UserCode: LoginData.userCode,
-    UserPwd: LoginData.userPwd,
+  console.error('LoginData:', LoginData)
+  getLogin({
     code: LoginData.code,
     state: LoginData.status,
   }).then((res) => {
@@ -55,6 +58,7 @@ function submit() {
       router.push('/')
     }
     else {
+      handleApiSuccessNotExcept(res)
       console.error(res)
     }
   })
@@ -64,8 +68,8 @@ function submit() {
 
 <template>
   <van-form mt-12vw flex flex-col items-end @submit="submit">
-    <van-field v-model="LoginData.userCode" label="用户名" placeholder="用户名长度为3到10位" />
-    <van-field v-model="LoginData.userPwd" type="password" label="密码" placeholder="密码长度为8到12位" />
+    <van-field v-model="LoginData.userCode" label="用户名" placeholder="用户名" />
+    <van-field v-model="LoginData.userPwd" type="password" label="密码" placeholder="密码" />
     <footer mt-12vw flex flex-row-reverse>
       <button class="btn" native-type="submit">
         登陆
